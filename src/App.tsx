313c7a119f5521d613d801a701272f2fa6d05620
Guido Juliano Ubaldi <guidojulianou@gmail.com>
2023-05-16 00:45:19 -0300

    Finalizada las funcionalidades del juego en su version beta

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import Board from "./components/Board";
-import { EmptyCell } from "./types/types";
+import UpcomingBlocks from "./components/UpcomingBlocks";
 import { useTetris } from "./hooks/useTetris";
 
 function App() {
-  const { board, startGame, isPlaying } = useTetris();
+  const { board, startGame, isPlaying, score, upcomingBlocks } = useTetris();
 
   return (
-    <div className="App">
+    <div className="app">
       <h1>Tetris</h1>
       <Board currentBoard={board} />
+      <div className="controls">
+        <h2>Score: {score}</h2>
+        {isPlaying ? (
+          <UpcomingBlocks upcomingBlocks={upcomingBlocks} />
+        ) : (
+          <button onClick={startGame}>New Game</button>
+        )}
+      </div>
     </div>
   );
 }
--- src/hooks/useTetrisBoard.ts
+++ src/hooks/useTetrisBoard.ts
@@ -1,11 +1,11 @@
+import { useReducer, Dispatch } from "react";
 import {
   Block,
   BlockShape,
   BoardShape,
-  SHAPES,
   EmptyCell,
+  SHAPES,
 } from "../types/types";
-import { useReducer, Dispatch } from "react";
 
 export const BOARD_WIDTH = 10;
 export const BOARD_HEIGHT = 20;
@@ -43,7 +43,32 @@ export function useTetrisBoard(): [BoardState, Dispatch<Action>] {
 export function getEmptyBoard(height = BOARD_HEIGHT): BoardShape {
   return Array(height)
     .fill(null)
-    .map(() => Array(BOARD_HEIGHT).fill(EmptyCell.Empty));
+    .map(() => Array(BOARD_WIDTH).fill(EmptyCell.Empty));
+}
+
+export function hasCollisions(
+  board: BoardShape,
+  currentShape: BlockShape,
+  row: number,
+  column: number
+): boolean {
+  let hasCollision = false;
+  currentShape
+    .filter((shapeRow) => shapeRow.some((isSet) => isSet))
+    .forEach((shapeRow: boolean[], rowIndex: number) => {
+      shapeRow.forEach((isSet: boolean, colIndex: number) => {
+        if (
+          isSet &&
+          (row + rowIndex >= board.length ||
+            column + colIndex >= board[0].length ||
+            column + colIndex < 0 ||
+            board[row + rowIndex][column + colIndex] !== EmptyCell.Empty)
+        ) {
+          hasCollision = true;
+        }
+      });
+    });
+  return hasCollision;
 }
 
 export function getRandomBlock(): Block {
@@ -51,14 +76,37 @@ export function getRandomBlock(): Block {
   return blockValues[Math.floor(Math.random() * blockValues.length)] as Block;
 }
 
+function rotateBlock(shape: BlockShape): BlockShape {
+  const rows = shape.length;
+  const columns = shape[0].length;
+
+  const rotated = Array(rows)
+    .fill(null)
+    .map(() => Array(columns).fill(false));
+
+  for (let row = 0; row < rows; row++) {
+    for (let column = 0; column < columns; column++) {
+      rotated[column][rows - 1 - row] = shape[row][column];
+    }
+  }
+
+  return rotated;
+}
+
 type Action = {
   type: "start" | "drop" | "commit" | "move";
+  newBoard?: BoardShape;
+  newBlock?: Block;
+  isPressingLeft?: boolean;
+  isPressingRight?: boolean;
+  isRotating?: boolean;
 };
 
 function boardReducer(state: BoardState, action: Action): BoardState {
-  let newState = { ...state };
+  const newState = { ...state };
+
   switch (action.type) {
-    case "start":
+    case "start": {
       const firstBlock = getRandomBlock();
       return {
         board: getEmptyBoard(),
@@ -67,14 +115,49 @@ function boardReducer(state: BoardState, action: Action): BoardState {
         droppingBlock: firstBlock,
         droppingShape: SHAPES[firstBlock].shape,
       };
-    case "drop":
+    }
+    case "drop": {
       newState.droppingRow++;
       break;
-    case "commit":
-    case "move":
-    default:
-    //const unhandledType: never = action.type;
-    //throw new Error(`Unhandled action type: ${unhandledType}`);
+    }
+
+    case "commit": {
+      return {
+        board: [
+          ...getEmptyBoard(BOARD_HEIGHT - action.newBoard!.length),
+          ...action.newBoard!,
+        ],
+        droppingRow: 0,
+        droppingColumn: 3,
+        droppingBlock: action.newBlock!,
+        droppingShape: SHAPES[action.newBlock!].shape,
+      };
+    }
+
+    case "move": {
+      const rotatedShape = action.isRotating
+        ? rotateBlock(newState.droppingShape)
+        : newState.droppingShape;
+      let columnOffset = action.isPressingLeft ? -1 : 0;
+      columnOffset = action.isPressingRight ? 1 : columnOffset;
+      if (
+        !hasCollisions(
+          newState.board,
+          rotatedShape,
+          newState.droppingRow,
+          newState.droppingColumn + columnOffset
+        )
+      ) {
+        newState.droppingColumn += columnOffset;
+        newState.droppingShape = rotatedShape;
+      }
+      break;
+    }
+
+    default: {
+      const unhandledType: never = action.type;
+      throw new Error(`Unhandled action type: ${unhandledType}`);
+    }
   }
 
   return newState;
